7cf31f3db60780c2abdd8194d959a6e09043cbd3
dwithanage <thisaraw@gmail.com>
2026-02-14 14:27:53 +1100

    minor updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-hello-world');
+  private readonly originalTitle = 'Angular Hello World from Don';
+  protected readonly title = signal(this.originalTitle);
+  protected inputValue = signal('');
+
+  onButtonClick() {
+    const inputText = this.inputValue().trim();
+    
+    if (inputText === '') {
+      alert('Please enter a text in the input field');
+      this.title.set(this.originalTitle);
+      return;
+    }
+    
+    this.title.set(inputText);
+  }
 }
+
+
